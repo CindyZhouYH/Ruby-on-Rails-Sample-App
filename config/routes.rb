ee0a3fab32beb22b241032bc2b9227a1e4240373
EC2 Default User <ec2-user@ip-172-31-86-1.ec2.internal>
2020-05-21 09:27:35 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  #get 'users/new'
-  #get 'user/new'
   root 'static_pages#home'
   get 'static_pages/home'
   get '/help', to: 'static_pages#help'
@@ -8,5 +6,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
